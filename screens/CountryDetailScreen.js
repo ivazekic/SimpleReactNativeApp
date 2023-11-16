27a9f34d6f9348be9959e5d8182d1df4ef223157
Iva Zekic <iva.zekic365@gmail.com>
2023-11-16 15:36:21 +0100

    Fetching details

--- screens/CountryDetailScreen.js
+++ screens/CountryDetailScreen.js
@@ -1,12 +1,41 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_DATA = gql`
+  query GetCountryDetails($countryName: StringQueryOperatorInput) {
+    countries(filter: { name: $countryName }) {
+      name
+      capital
+      currency
+      languages {
+        name
+      }
+      phone
+      emoji
+    }
+  }
+`;
 
 const CountryDetailScreen = ({ route }) => {
   const { name } = route.params;
 
+  const { loading, error, data } = useQuery(GET_DATA, {
+    variables: { countryName: { eq: name } },
+  });
+
+  const country = data ? data.countries[0] : {};
+
+  if (loading) return <ActivityIndicator size="large" color="#0000ff" />;
+  if (error) return <Text>Error: {error.message}</Text>;
+
   return (
     <View>
-      <Text>Details for country {name} </Text>
+      <Text>{country.emoji} </Text>
+      <Text>{country.name} </Text>
+      <Text>{country.capital} </Text>
+      <Text>{country.currency} </Text>
+      <Text>{country.phone} </Text>
     </View>
   );
 };
